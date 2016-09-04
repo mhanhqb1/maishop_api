2a25b9c66477070100b27ff27f5a3fdcb1419e50
Anh Mai <anh.mai@efe.com.vn>
2016-09-04 11:26:27 +0700

    Add database

---
+++ maishop.sql
@@ -0,0 +1,212 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50505
+Source Host           : localhost:3306
+Source Database       : maishop
+
+Target Server Type    : MYSQL
+Target Server Version : 50505
+File Encoding         : 65001
+
+Date: 2016-09-04 11:24:11
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for attributes
+-- ----------------------------
+DROP TABLE IF EXISTS `attributes`;
+CREATE TABLE `attributes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `disable` tinyint(4) DEFAULT NULL,
+  `created` int(11) DEFAULT NULL,
+  `updated` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for categories
+-- ----------------------------
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `position` int(11) DEFAULT NULL,
+  `disable` tinyint(4) DEFAULT '0',
+  `created` int(11) DEFAULT NULL,
+  `updated` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for districts
+-- ----------------------------
+DROP TABLE IF EXISTS `districts`;
+CREATE TABLE `districts` (
+  `districtid` varchar(5) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `type` varchar(30) NOT NULL,
+  `location` varchar(30) NOT NULL,
+  `provinceid` varchar(5) NOT NULL,
+  PRIMARY KEY (`districtid`),
+  KEY `provinceid` (`provinceid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for orders
+-- ----------------------------
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `phone` varchar(20) DEFAULT NULL,
+  `note` text,
+  `address` varchar(255) DEFAULT NULL,
+  `district_id` int(11) DEFAULT NULL,
+  `province_id` int(11) DEFAULT NULL,
+  `total_price` float DEFAULT NULL,
+  `is_paid` tinyint(4) DEFAULT '0',
+  `is_cancle` tinyint(4) DEFAULT '0',
+  `disable` tinyint(4) DEFAULT '0',
+  `created` int(11) DEFAULT NULL,
+  `updated` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for order_products
+-- ----------------------------
+DROP TABLE IF EXISTS `order_products`;
+CREATE TABLE `order_products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `order_id` int(11) DEFAULT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `created` int(11) DEFAULT NULL,
+  `updated` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for products
+-- ----------------------------
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cate_id` int(11) NOT NULL,
+  `price` decimal(10,0) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `detail` text,
+  `is_feature` tinyint(4) DEFAULT '0',
+  `disable` tinyint(4) DEFAULT NULL,
+  `created` int(11) DEFAULT NULL,
+  `updated` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for product_attributes
+-- ----------------------------
+DROP TABLE IF EXISTS `product_attributes`;
+CREATE TABLE `product_attributes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) DEFAULT NULL,
+  `attribute_id` int(11) DEFAULT NULL,
+  `value` varchar(255) DEFAULT NULL,
+  `created` int(11) DEFAULT NULL,
+  `updated` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for product_images
+-- ----------------------------
+DROP TABLE IF EXISTS `product_images`;
+CREATE TABLE `product_images` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `is_default` tinyint(4) DEFAULT NULL,
+  `created` int(11) DEFAULT NULL,
+  `updated` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for provinces
+-- ----------------------------
+DROP TABLE IF EXISTS `provinces`;
+CREATE TABLE `provinces` (
+  `provinceid` varchar(5) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `type` varchar(30) NOT NULL,
+  PRIMARY KEY (`provinceid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for sub_categories
+-- ----------------------------
+DROP TABLE IF EXISTS `sub_categories`;
+CREATE TABLE `sub_categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `position` int(11) DEFAULT NULL,
+  `cate_id` int(11) DEFAULT NULL,
+  `disable` tinyint(4) DEFAULT '0',
+  `created` int(11) DEFAULT NULL,
+  `updated` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `district_id` varchar(5) DEFAULT NULL,
+  `province_id` varchar(5) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `avatar` varchar(255) DEFAULT NULL,
+  `birthday` date DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `is_admin` tinyint(4) DEFAULT '0',
+  `count_product` int(11) DEFAULT NULL,
+  `disable` tinyint(4) DEFAULT '0',
+  `created` int(11) DEFAULT NULL,
+  `updated` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for user_products
+-- ----------------------------
+DROP TABLE IF EXISTS `user_products`;
+CREATE TABLE `user_products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `created` int(11) DEFAULT NULL,
+  `updated` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for wards
+-- ----------------------------
+DROP TABLE IF EXISTS `wards`;
+CREATE TABLE `wards` (
+  `wardid` varchar(5) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `type` varchar(30) NOT NULL,
+  `location` varchar(30) NOT NULL,
+  `districtid` varchar(5) NOT NULL,
+  PRIMARY KEY (`wardid`),
+  KEY `districtid` (`districtid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
